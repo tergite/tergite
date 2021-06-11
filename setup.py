65ecfa388e84a5769658da0bedfd0168a8979a22
Miroslav Dobsicek <dobsicek@chalmers.se>
2021-06-11 15:44:04 +0200

    [BC-18] Add setup.py to the package
    > Description
    -------------
    
    The setup.py file takes care of:
     * adding the package to Python module search paths
     * installation dependencies
    
    The expected package installation is in the development mode with
    
      pip install -e .
    
    The minimal required Python version is 3.8.
    
    > Verification
    --------------
    Functionality has been verified with the whole stack:
    Qiskit, MSS, BCC, Labber.

---
+++ setup.py
@@ -0,0 +1,30 @@
+# This code is part of Tergite
+#
+# (C) Copyright Miroslav Dobsicek 2021
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+
+from setuptools import setup, find_packages
+
+REQUIREMENTS = [
+    "requests>=2.25.1",
+    "python_dateutil>=2.8.1",
+    "python-multipart>=0.0.5",
+    "qiskit-terra>=0.17.4",
+]
+
+setup(
+    name="tergite-qiskit-connector",
+    author_emails="dobsicek@chalmers.se",
+    license="Apache 2.0",
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+    python_requires=">=3.8",
+)
