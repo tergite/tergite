7e2ff71873abb379f40f2b979c14201510613acd
Axel Andersson <axean@student.chalmers.se>
2022-03-25 10:16:13 +0100

    Removed other backends

--- qiskit/providers/tergite/hardcoded_backend_data.py
+++ qiskit/providers/tergite/hardcoded_backend_data.py
@@ -12,49 +12,6 @@ from typing import Optional, List, Union, Iterable, Tuple
 
 import functools
 
-class Thor(Backend):
-    def __init__(self, provider, base_url):
-        super().__init__(
-            provider        = provider,
-            base_url        = base_url,
-            name            = "Thor",
-            description     = "Connected to the Chalmers intranet.",
-            online_date     = datetime.now(),
-            backend_version = "0.5.4", # QBLOX firmware driver version
-        )
-    
-    @property
-    def target(self) -> Target:
-        gmap = Target(num_qubits = self.coupling_map.size(), dt = 1e-9)
-        
-        # TODO: gate calibrations
-        
-        return gmap 
-    
-    @property
-    def qubit_lo_freq(self) -> List[float]:
-        return [f*1e9 for f in (4.1,4.2,4.3,4.4,4.5)]
-    
-    @property
-    def meas_lo_freq(self) -> List[float]:
-        return [f*1e9 for f in (6.1,6.2,6.3,6.4,6.5)]
-    
-    @property
-    def meas_map(self) -> List[List[int]]:
-        return [[i for i in range( self.num_qubits )]]
-    
-    @property
-    def coupling_map(self) -> CouplingMap:
-        cl = [
-            [0,1], [1,0],
-            [0,2], [2,0],
-            [0,3], [3,0],
-            [0,4], [4,0]
-        ]
-        return CouplingMap(couplinglist = cl)
-
-#------------------------------------------------------------------------------------------------------
-
 class Pingu(Backend):
     def __init__(self, provider, base_url):
         super().__init__(
@@ -143,11 +100,11 @@ class Pingu(Backend):
     
     @property
     def qubit_lo_freq(self) -> List[float]:
-        return [f*1e9 for f in (4.1395428, 4.773580)]
+        return [4.1395428e9, 4.773580e9 + 1.3e6]
     
     @property
     def meas_lo_freq(self) -> List[float]:
-        return [f*1e9 for f in (6.21445, 6.379170)]
+        return [6.21445e9, 6.379170e9]
     
     @property
     def meas_map(self) -> List[List[int]]:
@@ -162,42 +119,4 @@ class Pingu(Backend):
 
 #------------------------------------------------------------------------------------------------------
 
-class Test(Backend):
-    def __init__(self, provider, base_url):
-        super().__init__(
-            provider        = provider,
-            base_url        = base_url,
-            name            = "Test",
-            description     = "Test object for debugging. Five fake qubits.",
-            online_date     = datetime.now(),
-            backend_version = "0.0.0"
-        )
-    
-    @property
-    def target(self) -> Target:
-        gmap = Target(num_qubits = self.coupling_map.size(), dt = 1e-9)
-        
-        # TODO: gate calibrations
-        
-        return gmap 
-    
-    @property
-    def qubit_lo_freq(self) -> List[float]:
-        return [f for f in range(self.num_qubits)]
-    
-    @property
-    def meas_lo_freq(self) -> List[float]:
-        return [(f+1)*10 for f in range(self.num_qubits)]
-    
-    @property
-    def meas_map(self) -> List[List[int]]:
-        return [[i for i in range( self.num_qubits )]]
-    
-    @property
-    def coupling_map(self) -> CouplingMap:
-        cl = list(permutations([i for i in range(5)], 2))
-        return CouplingMap(couplinglist = cl)
-
-#------------------------------------------------------------------------------------------------------
-
-hardcoded_backends = [ Thor, Pingu, Test ]
+hardcoded_backends = [ Pingu ]
