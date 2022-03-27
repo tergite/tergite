2f34fc25ed4315a7153e10864fe1b7907c094e7a
Axel Andersson <axean@student.chalmers.se>
2022-03-27 17:19:46 +0200

    Implemented source basis: {rx, rz, measure, delay} based on calibrations (untested)

--- qiskit/providers/tergite/hardcoded_backend_data.py
+++ qiskit/providers/tergite/hardcoded_backend_data.py
@@ -1,14 +1,30 @@
 from .backend import Backend
 from datetime import datetime
-from qiskit.transpiler import Target
+from qiskit.transpiler import Target, InstructionProperties
 from qiskit.transpiler.coupling import CouplingMap
 from qiskit.pulse import Schedule, ScheduleBlock, InstructionScheduleMap
 from qiskit.pulse.instructions import Play, Delay, SetFrequency, SetPhase, ShiftFrequency, ShiftPhase, Acquire
 #from qiskit.pulse.channels import DriveChannel, MeasureChannel, AcquireChannel, ControlChannel
 from qiskit.pulse.library import Gaussian, Constant, cos, GaussianSquare
+
+#from qiskit.circuit import QuantumCircuit
+from qiskit.circuit import Delay as circuitDelay
+
+from qiskit.circuit.measure import Measure
+from qiskit.circuit.library.standard_gates import (
+    CXGate,
+    UGate,
+    ECRGate,
+    RXGate,
+    SXGate,
+    XGate,
+    RZGate,
+)
 from qiskit.circuit import Parameter, Instruction
 from itertools import permutations
 from typing import Optional, List, Union, Iterable, Tuple
+from numpy import sin
+import pandas as pd
 
 import functools
 
@@ -22,80 +38,119 @@ class Pingu(Backend):
             online_date     = datetime.now(),
             backend_version = "0.5.4", # QBLOX firmware driver version
         )
+        self._dt = 1e-9
+        
+    @functools.cached_property
+    def calibration_table(self: object):
+        # This should be returned from a database, but right now this suffices
+        # all values should be in SI units
+        df = pd.DataFrame()
+        df["qubit"] = [0, 1]
+        df.set_index("qubit")
+        
+        df["rabi_amp_gauss"]           = [0.1    , 0.0994]
+        df["rabi_dur_gauss"]           = [58e-9  , 58e-9 ]
+        
+        df["readout_amp_square"]       = [14e-3  , 14e-3 ]
+        df["readout_dur_square"]       = [3.5e-6 , 3.5e-6]
+        df["readout_integration_time"] = [3e-6   , 3e-6  ]
+        df["readout_tof"]              = [300e-9 , 300e-9]
+        return df
+        
+    def __getitem__(
+        self: object,
+        args: tuple
+    ):        
+        gate_name = args[0]
+        qubits = args[1]
+        params = args[2:]
+        get_param = lambda name: next(filter(lambda param: param.name == name, params))
+        
+        sched = ScheduleBlock(gate_name)
+        if gate_name == "rx":
+            θ = get_param("theta")
+            for qubit in qubits:
+                sched += Play(Constant(
+                    amp = sin(θ/2) * self.calibration_table["rabi_amp_gauss"][qubit],
+                    duration = round(self.calibration_table["rabi_dur_gauss"][qubit]/self._dt)
+                ), self.drive_channel(qubit))
+        
+        elif gate_name == "rz":
+            λ = get_param("lambda")
+            for qubit in qubits:
+                sched += ShiftPhase(λ, self.drive_channel(qubit))
+            
+        elif gate_name == "measure":
+            for qubit in qubits:
+
+                sched += Play(Constant(
+                    amp = self.calibration_table["readout_amp_square"][qubit],
+                    duration = round(
+                        self.calibration_table["readout_dur_square"][qubit] / self._dt
+                    )
+                ), self.measure_channel(qubit))
+                
+                sched += Delay(
+                    duration = round(
+                        self.calibration_table["readout_tof"][qubit] / self._dt
+                    ),
+                    channel = self.acquire_channel(qubit)
+                )
+                
+                sched += Acquire(
+                    duration = round(
+                        self.calibration_table["readout_integration_time"][qubit] / self._dt
+                    ),
+                    channel = self.acquire_channel(qubit),
+                    mem_slot = self.memory_slot(qubit) # FIXME: deranged c-map is broken
+                )
+        
+        elif gate_name == "delay":
+            τ = get_param("tau")
+            for qubit in qubits:
+                sched += Delay(
+                    duration = τ,
+                    channel = self.drive_channel(qubit)
+                )
+        
+        else:
+            raise NotImplementedError(f"Calibration for gate {gate_name} has not yet been added to {self.name}.")
+
+        return sched
         
     @functools.cached_property
     def target(self) -> Target:
-        num_qubits = self.coupling_map.size()
-        gmap = Target(num_qubits = num_qubits, dt = 1e-9)
-        ismap = InstructionScheduleMap()
-        """
-            How to add a new gate definition:
-                - Experimentally find numerical parameters for the pulse.
-                - Create a minimal ScheduleBlock which implements the instruction on this backend.
-                - Use InstructionScheduleMap.add to add that ScheduleBlock to the 'ismap'.
-                - Create an Instruction object which lets the compiler know how to use it.
-        """
-        params = {
-            k : Parameter(k) for k in (
-                "qubit",
-                "x_dur",
-                "x_amp",
-                "x_sig",
-            )
+
+        gmap = Target(num_qubits = self.coupling_map.size(), dt = self._dt)
+        
+        θ = Parameter("theta")  # rotation around x axis
+        φ = Parameter("phi")    # rotation around y axis
+        λ = Parameter("lambda") # rotation around z axis
+        τ = Parameter("tau")    # duration
+        
+        rx_props = {
+            (0,): InstructionProperties(error=0.0, calibration = self["rx", (0,), θ]),
+            (1,): InstructionProperties(error=0.0, calibration = self["rx", (1,), θ]),
         }
-        gates = dict()
+        gmap.add_instruction(RXGate(θ), rx_props)
         
-        # temp solution to assign parameters
-        calibrations = {
-            0: dict(
-                qubit = 0,
-                x_dur =0, #FIXME
-                x_amp =0, #FIXME
-                x_sig =1, #FIXME
-            ),
-            1 : dict(
-                qubit = 1,
-                x_dur = 120,
-                x_amp = 0.0904,
-                x_sig = 40,
-            )
+        rz_props = {
+            (0,): InstructionProperties(error=0.0, calibration = self["rz", (0,), λ]),
+            (1,): InstructionProperties(error=0.0, calibration = self["rz", (1,), λ]),
         }
+        gmap.add_instruction(RZGate(λ), rz_props)
         
-        gate_defs = list()
+        delay_props = {
+            (0,): InstructionProperties(error=0.0, calibration = self["delay", (0,), τ]),
+            (1,): InstructionProperties(error=0.0, calibration = self["delay", (1,), τ]),
+        }
+        gmap.add_instruction(circuitDelay(τ, unit = "dt"), delay_props)
         
-        # ================== DEFINE OPERATIONS ================= #
-        # ------------------------- x -------------------------- #
-        x_gate = ScheduleBlock(name = "x")
-        x_gate += Play(
-            Gaussian(
-                params["x_dur"],
-                amp = params["x_amp"],
-                sigma = params["x_sig"]
-            ),
-            self.drive_channel(params["qubit"]),
-            name = "x gate"
-        )
+        measure_props = {
+            (0,1): InstructionProperties(error=0.0, calibration = self["measure", (0,1)]),
+        }
+        gmap.add_instruction(Measure(), measure_props)
         
-        for qubit_idx in range(num_qubits):
-            ismap.add("x", qubit_idx, x_gate.assign_parameters({
-                    params[k] : calibrations[qubit_idx][k] for k in params
-                },
-                inplace = False
-            ))
-            
-        gates["x"] = Instruction(
-            name = "x",
-            num_qubits = num_qubits,
-            num_clbits = 0,
-            params = [],
-            duration = 200,
-            unit = "dt",
-            label = "x gate"
-        )
-        # ------------------------ /x -------------------------- #
-        
-        # ================= /DEFINED OPERATIONS ================ #
-        gmap.update_from_instruction_schedule_map(ismap, inst_name_map = gates)
         return gmap
     
     @property
