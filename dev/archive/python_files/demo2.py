8b186af94d13ec5a93bf4160bbf58bd895e6c962
Axel Andersson <axean@chalmers.se>
2022-11-14 11:22:51 +0100

    Removed duplicates and binaries from tests folder. Archived scripts and notebooks inside dev/ instead since I want to reserve tests/ for pytest automated testing.

---
+++ dev/archive/python_files/demo2.py
@@ -0,0 +1,13 @@
+from pprint import pprint
+
+from qiskit.visualization import plot_histogram
+
+from qiskit import *
+from qiskit.providers.tergite import Tergite
+
+provider = Tergite.get_provider()
+pingu_backend = provider.get_backend("pingu")
+
+
+quantum_circuit = QuantumCircuit.from_qasm_file("tests/example.qasm")
+print(quantum_circuit)
---
+++ dev/archive/python_files/multi_qubit_rotations.py
@@ -0,0 +1,75 @@
+from pprint import pprint
+
+import qiskit.quantum_info as qi
+from qiskit.ignis.verification.tomography import (
+    StateTomographyFitter,
+    state_tomography_circuits,
+)
+from qiskit.visualization import (
+    plot_bloch_multivector,
+    plot_histogram,
+    plot_state_city,
+    plot_state_hinton,
+    plot_state_paulivec,
+    plot_state_qsphere,
+)
+
+from qiskit import *
+from qiskit.providers.tergite import Tergite
+
+#
+# Note: This code has been used on "Quantum software" PoC hearing
+# on January 25, 2022.
+# Run with: python -i multi_qubit_rotations.py
+#
+
+# Backend ------------------------------------------------------
+
+# Tergite
+provider = Tergite.get_provider()
+backend = provider.get_backend("pingu")
+
+# Aer simulator
+# backend = Aer.get_backend("aer_simulator")
+
+# IBM
+# account = IBMQ.load_account()
+# backend= account.get_backend("ibmq_belem")
+
+
+# Circuit ------------------------------------------------------
+q = QuantumRegister(3)
+circ = QuantumCircuit(q)
+circ.h(q[0])
+circ.h(q[1])
+circ.s(q[1])
+circ.x(q[2])
+
+print("Input circuit")
+print(circ)
+
+# State tomography circuits ------------------------------------
+target_state = qi.Statevector.from_instruction(circ)
+tomography_circuits = state_tomography_circuits(circ, [q[0], q[1], q[2]])
+
+print("State tomography circuits")
+print(tomography_circuits[0])
+print("")
+print(tomography_circuits[1])
+print("")
+print(tomography_circuits[2])
+
+
+# Execute ------------------------------------------------------
+job = execute(tomography_circuits, backend, shots=1000)
+
+input("Executing job, press enter once it's finished")
+
+
+# Fit  ---------------------------------------------------------
+fitter = StateTomographyFitter(job.result(), tomography_circuits)
+density_matrix = fitter.fit(method="lstsq")
+plot_bloch_multivector(density_matrix).show()
+
+fidelity = qi.state_fidelity(density_matrix, target_state)
+print("State Fidelity: F = {:.5f}".format(fidelity))
