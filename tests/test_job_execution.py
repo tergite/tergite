3f0e07b43530194d4369bf8279277d020918f348
Stefan Hill <stefanhi@chalmers.se>
2023-12-06 13:13:48 +0100

    Updated frequencies for the test_transpile according to the new backend fixture

--- tests/test_job_execution.py
+++ tests/test_job_execution.py
@@ -465,10 +465,10 @@ def _get_expected_transpiled_circuit():
         name="RX(θ, (1,))",
         alignment_context=pulse.transforms.AlignLeft(),
     )
-    rx_block.append(pulse.SetFrequency(3304746060, pulse.DriveChannel(1)))
+    rx_block.append(pulse.SetFrequency(3932312578.2853203, pulse.DriveChannel(1)))
     rx_block.append(
         pulse.Play(
-            pulse.Gaussian(duration=52, amp=(0.03308 + 0j), sigma=6, name="RX q1"),
+            pulse.Gaussian(duration=52, amp=(0.0290173467+0j), sigma=6, name="RX q1"),
             pulse.DriveChannel(1),
             name="RX q1",
         )
